53be155d17c681d4a6339fd2b3a194e045beaca7
Katrina Tran <kat.c.tran@gmail.com>
2018-11-11 10:14:21 -0700

    choose player

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Player from './components/choosePlayer';
 
 
 class App extends Component {
@@ -7,18 +8,48 @@ class App extends Component {
     super(props)
     this.state = {
       board : Array(9).fill(null),
-      player : "X"
+      player : "X",
+      winner : null
     }
   }
+
+
+  checkWinner() {
+    let winLines =
+    [
+      ["0", "1", "2"],
+      ["3", "4", "5"],
+      ["6", "7", "8"],
+      ["0", "3", "6"],
+      ["1", "4", "7"],
+      ["2", "5", "8"],
+      ["0", "4", "8"],
+      ["2", "4", "6"]
+    ]
+
+    for (let index = 0; index < winLines.length; index++) {
+      const [a, b, c] = winLines[index];
+      if (this.state.board[a] && this.state.board[a] === this.state.board[b] && this.state.board[a] === this.state.board[c]) {
+        alert('You won');
+        this.setState({
+          winner: this.state.player
+        })
+      }
+
+    }
+  }
+
   handleClick(index) {
 
     let newBoard = this.state.board
-    if(this.state.board[index] === null){
+    if(this.state.board[index] === null && !this.state.winner) {
       newBoard[index] = this.state.player
       this.setState({
         board: newBoard,
         player : this.state.player === "X" ? "O" : "X"
       })
+
+      this.checkWinner()
     }
   }
 
@@ -34,6 +65,7 @@ class App extends Component {
     return (
       <div className="container">
         <h1> Tic Tac Toe</h1>
+        <Player />
         <div className="board">
           {Box}
         </div>
