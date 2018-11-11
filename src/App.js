a8d57b1a5cd4ccbf03c19b1848d44088c8610e58
Katrina Tran <kat.c.tran@gmail.com>
2018-11-10 22:25:04 -0700

    created x

--- src/App.js
+++ src/App.js
@@ -3,11 +3,39 @@ import './App.css';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      board : Array(9).fill(null),
+    }
+  }
+  handleClick(index) {
+
+    let newBoard = this.state.board
+    newBoard[index] = "X"
+    this.setState({
+      board: newBoard
+    })
+    console.log(this.state.board)
+
+    // console.log(index)
+  }
+
   render() {
+    const Box = this.state.board.map(
+      (box, index) =>
+      <div className="box"
+      key={index}
+      onClick={() => this.handleClick(index)}>
+      {box}
+      </div>
+    )
     return (
       <div className="container">
         <h1> Tic Tac Toe</h1>
-        <div className="box"></div>
+        <div className="board">
+          {Box}
+        </div>
       </div>
     );
   }
